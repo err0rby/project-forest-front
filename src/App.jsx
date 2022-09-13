e62dca1e4148ce79c3ed883074c3f9cd22a6fb03
Saluev <ibragim.saluev@mail.ru>
2022-09-13 14:49:51 +0300

    Хэдар готов

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import SignIn from "./components/Auth/SignIn";
 import SignUp from "./components/Auth/SignUp";
 import Home from "./components/Home/Home";
+import Headers from "./components/Headers/Headers";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Home/>}/>
       <Route path="/signin" element={<SignIn/>}/>
       <Route path="/signup" element={<SignUp/>}/>
+      <Route path="/header" element={<Headers/>}/>
     </Routes>
   );
 }
