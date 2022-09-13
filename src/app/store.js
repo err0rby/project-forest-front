b5ca49bad6f6ede3430632dea5345532a00f754e
errorby <mdidayev@inbox.ru>
2022-09-13 15:52:02 +0300

    Сделал админа и коммы

--- src/app/store.js
+++ src/app/store.js
@@ -3,13 +3,15 @@ import applicationSlice from "../features/applicationSlice";
 import workersSlice from "../features/workersSlice";
 import requestSlice from "../features/requestSlice";
 import serviceSlice from "../features/serviceSlice";
+import commentSlice from "../features/commentSlice";
 
 const store = configureStore({
     reducer: {
         applicationSlice,
         workersSlice,
         requestSlice,
-        serviceSlice
+        serviceSlice,
+        commentSlice,
     }
 });
 
