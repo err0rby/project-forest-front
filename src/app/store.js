8b78ce852468f5653b28aea485220c3b55666bcc
errorby <mdidayev@inbox.ru>
2022-09-13 13:58:43 +0300

    пока не всё

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import applicationSlice from "../features/applicationSlice";
 import workersSlice from "../features/workersSlice";
+import requestSlice from "../features/requestSlice";
 
 const store = configureStore({
     reducer: {
         applicationSlice,
-        workersSlice
+        workersSlice,
+        requestSlice,
     }
 });
 
