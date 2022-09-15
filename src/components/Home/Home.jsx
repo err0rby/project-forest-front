fe32fa10988e7a278d3baa8ac53f2251a554b2b5
MovsarTS <mvs451@mail.ru>
2022-09-15 16:30:26 +0300

    +++

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,14 +2,19 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addComms, fetchComms } from '../../features/commentSlice';
 import style from './Home.module.css'
+import image from '../Home/image/image.jpg'
 import { Link } from 'react-router-dom';
 import Slider from './Slider';
+import { motion } from 'framer-motion';
+import Maps from '../Maps/Maps';
 
 const Home = () => {
     const [text, setText] = useState('');
     const dispatch = useDispatch();
     const name = useSelector(state => state.applicationSlice.name);
     const comments = useSelector(state => state.commentSlice.comments);
+    const count = comments.length
+    console.log(comments.length)
 
     useEffect(() => {
         dispatch(fetchComms());
@@ -32,15 +37,30 @@ const Home = () => {
                     <Link to='/service' className={style.buttonAbout1}>Услуги</Link>
                 </div>
             </div>
-            <div>{comments.map((com) => {
-                return <div key={com._id}>
-                    <div>{com.user.login}</div>
-                    <div>{com.text}</div>
-                </div>
-            })}
+            <div className={style.main_text}>
+                <p>Отзывы наших клиентов</p>
             </div>
-            <textarea value={text} onChange={(e) => setText(e.target.value)}></textarea>
-            <button onClick={() => { handleSend() }}>send comment</button>
+            <motion.div className={style.main_comments}>
+                <motion.div drag='x' dragConstraints={{right: 0, left: count * -365}} className={style.main_comments_comments}>
+                {comments.map((com) => {
+                return <motion.div className={style.card_comments} key={com._id}>
+                            <div className={style.card_comments1}><p>{com.user.login}</p></div>
+                            <div className={style.card_comments2}><p>{com.text}</p></div>
+                        </motion.div>
+            })}
+                </motion.div>
+            </motion.div>
+            <div className={style.main_form}>
+                <p>Вы можете оставить свой отзыв:</p>
+                <div className={style.form}>
+                    <div className={style.form_input}><input value={text} onChange={(e) => setText(e.target.value)}></input></div>
+                    <div className={style.form_btn}><button onClick={() => { handleSend() }}>Добавить комментарий</button></div>
+                </div>
+            </div>
+            <div className={style.main_text2}>
+                <p>Адрес нашей кампании</p>
+            </div>
+            <Maps />
         </div>
     );
 };
